9ae1f66eef9ec61cdb064ad4af6bcc081cdbe8bd
JayScriptt <corneliogatbontonjr21@gmail.com>
2025-09-12 00:59:07 +0800

    LOG IN MODAL DONE

---
+++ js/loginModal.js
@@ -0,0 +1,13 @@
+// const modal = document.getElementById("loginModal");
+// modal.addEventListener("click", (e) => {
+//   if (e.target === modal) {
+//     e.stopPropagation(); // disable background click
+//   }
+// });
+
+// // Simulate login success (for testing)
+// document.querySelector("form").addEventListener("submit", function (e) {
+//   e.preventDefault(); // remove this in real PHP login
+//   // if login success from backend PHP -> hide modal
+//   modal.style.display = "none";
+// });
